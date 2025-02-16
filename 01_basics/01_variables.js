28d682e5ebe9eedc3fba65ced2dff7b883b3bad2
usama1095854 <https://github.com/usama1095854>
2025-02-16 23:45:26 +0500

    learn variables and data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = '6546131';
+let accountEmail = 'usama@mail.com';
+var accountPassword = '123456';
+
+accountCity = 'Karachi';
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,38 @@
+'use strict';
+
+// if we are running js code using node it will not work
+// we are using nodejs, not browser
+// alert(3 + 3);
+
+console.log(3 + 3);
+console.log('usama');
+
+let name = 'Usama';
+let age = 26;
+let isLoggedIn = true;
+
+let temprature = null;
+temprature = 20;
+
+// Primitive Date Types
+//======================
+// Number ==> Range 2 to power 53
+// bigInt
+// string ==> ""
+// boolean ==> true/false
+// null ==> standalone value -----> representation empty value
+// undefined ==>
+// symbol ==> unique
+
+let state = null;
+console.log(state);
+
+let country; // undefined
+console.log(country);
+
+// Non-Primitve data types
+// ==========================
+
+console.log(typeof 'usama'); // ==> string
+console.log(typeof null); // ==> object
+console.log(typeof undefined); // ==> undefined
